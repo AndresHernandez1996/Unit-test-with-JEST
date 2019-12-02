eea377db9276f80cba5cfbb0a8283e0454c122b4
Andrés Hernández <yayodelrey@MacBook-Pro-de-Andres.local>
2019-12-01 21:58:40 -0600

    Routing, Redux setup

--- src/components/__test__/CommentBox.test.js
+++ src/components/__test__/CommentBox.test.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { mount } from 'enzyme'
 import CommentBox from 'components/CommentBox'
+import { wrap } from 'module'
 
 let wrapped
 
@@ -19,11 +20,24 @@ it('Has a text area and a button', () => {
   expect(wrapped.find('button').length).toEqual(1)
 })
 
-it('Has textarea that users can type in', () => {
-  wrapped.find('textarea').simulate('change', {
-    target: { value: 'new comment' }
+describe('the text area', () => {
+  beforeEach(() => {
+    wrapped.find('textarea').simulate('change', {
+      target: { value: 'new comment' }
+    })
+    wrapped.update()
   })
-  wrapped.update()
+  it('Has textarea that users can type in', () => {
+    expect(wrapped.find('textarea').prop('value')).toEqual('new comment')
+  })
 
-  expect(wrapped.find('textarea').prop('value')).toEqual('new comment')
+  it('When form is submitted, text area gets emptied', () => {
+    wrapped.find('textarea').simulate('change', {
+      target: { value: 'new comment' }
+    })
+    wrapped.update()
+    wrapped.find('form').simulate('submit')
+    wrapped.update()
+    expect(wrapped.find('textarea').prop('value')).toEqual('')
+  })
 })
